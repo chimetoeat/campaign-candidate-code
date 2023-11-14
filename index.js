7458dc2437c4bc63f16e132deeaceb33a746a34f
Vince <banongofficial28@gmail.com>
2023-11-15 00:39:51 +0800

    Created Unit Test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const ejs = require("ejs");
-const axios = require('axios');
-const methodOverride = require('method-override')
+import express from 'express'
+import ejs from 'ejs'
+import axios from 'axios'
+import methodOverride from 'method-override'
 
 const app = express();
 app.set("view engine", "ejs");
@@ -14,6 +14,62 @@ app.get("/", function (req, res) {
     res.redirect("/candidates");
 })
 
+// Create Candidate
+app.get('/create-candidate', function (req, res) {
+    res.render("candidate");
+})
+
+app.post('/create-candidate', async (req, res) => {
+    const { first_name, last_name, email } = req.body;
+
+    // POST REQUEST to Talkpush API to Create a new Candidate for Campaign ID 52
+    try {
+        const response = await axios.post('https://tech-eval.talkpush.com/api/talkpush_services/campaigns/52/campaign_invitations',
+        {
+            "api_key": "77a9113bda7ae5b92a6ef892135d4e04",
+            "campaign_invitation": {
+              "first_name": first_name,
+              "last_name": last_name,
+              "email": email,
+              "user_phone_number": "string",
+              "source": "string",
+              "others": {}
+            }
+        })
+        res.redirect("/candidates")
+    }
+    catch(err) {
+        res.render("errors", {err})
+    }
+});
+
+// Retrieve Candidates
+app.get('/candidates', async (req, res) => {
+    // Retrieve all Candidates from Campaign ID 52
+    try {
+        const response = await axios.get('https://tech-eval.talkpush.com/api/talkpush_services/campaign_invitations?api_key=77a9113bda7ae5b92a6ef892135d4e04&filter%5Bcampaign_id%5D=52')
+        const candidatesRaw = response.data.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>|/gi,'').replace(/&quot;/gi,'"')
+        const candidates = JSON.parse(candidatesRaw)
+        res.render('allCandidates', {candidates})
+    }
+    catch(err) {
+        res.render("errors", {err})
+    }
+})
+
+// Shortlist Candidates
+app.put('/shortlist', async (req, res) => {
+    const { shortlistButtonID } = req.body;
+
+    try {
+        const response = await axios.put("https://tech-eval.talkpush.com/api/talkpush_services/campaign_invitations/" + shortlistButtonID + "/shortlist?api_key=77a9113bda7ae5b92a6ef892135d4e04")
+        res.redirect('/candidates')
+    }
+    catch(err) {
+        res.render("errors", {err})
+    }
+})
+
 // Create Campaign
 app.get("/campaign", function (req, res) {
     res.render("campaign");
@@ -22,9 +78,9 @@ app.get("/campaign", function (req, res) {
 app.post('/campaign', async (req, res) => {
         const { campaign_title } = req.body;
 
-        // POST REQUEST to external JSON API to Create a new Campaign
+        // POST REQUEST to TalkPush API to Create a new Campaign
         try {
-            const result = await axios.post('https://tech-eval.talkpush.com/api/talkpush_services/campaigns',
+            const response = await axios.post('https://tech-eval.talkpush.com/api/talkpush_services/campaigns',
             {
                 "api_key": "77a9113bda7ae5b92a6ef892135d4e04",
                 "campaign": {
@@ -44,78 +100,16 @@ app.post('/campaign', async (req, res) => {
                     "salary_currency": "string"
                 }
             })
-            res.redirect("candidate")
-            console.log(result.data)
+            res.redirect('/candidates')
         }
         catch(err) {
             res.render("errors", {err})
-            console.log(err)
         }
   });
-
-
-// Create Candidate
-app.get('/create-candidate', function (req, res) {
-    res.render("candidate");
-})
-
-app.post('/create-candidate', async (req, res) => {
-    const { first_name, last_name, email } = req.body;
-
-    // POST REQUEST to external JSON API to Create a new Candidate for Campaign ID 52
-    try {
-        const result = await axios.post('https://tech-eval.talkpush.com/api/talkpush_services/campaigns/52/campaign_invitations',
-        {
-            "api_key": "77a9113bda7ae5b92a6ef892135d4e04",
-            "campaign_invitation": {
-              "first_name": first_name,
-              "last_name": last_name,
-              "email": email,
-              "user_phone_number": "string",
-              "source": "string",
-              "others": {}
-            }
-        })
-        res.redirect("/candidates")
-        console.log(result.data)
-    }
-    catch(err) {
-        res.render("errors", {err})
-        console.log(err)
-    }
-});
-
-// Retrieve Candidates
-app.get('/candidates', async (req, res) => {
-    // Retrieve all Candidates from Campaign ID 52
-    try {
-        const result = await axios.get('https://tech-eval.talkpush.com/api/talkpush_services/campaign_invitations?api_key=77a9113bda7ae5b92a6ef892135d4e04&filter%5Bcampaign_id%5D=52')
-        const candidatesRaw = result.data.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>|/gi,'').replace(/&quot;/gi,'"')
-        const candidates = JSON.parse(candidatesRaw)
-        res.render('allCandidates', {candidates})
-    }
-    catch(err) {
-        res.render("errors", {err})
-        console.log(err)
-    }
-})
-
-// Shortlist Candidates
-app.put('/shortlist', async (req, res) => {
-    const { shortlistButton } = req.body;
-
-    try {
-        const result = await axios.put("https://tech-eval.talkpush.com/api/talkpush_services/campaign_invitations/" + shortlistButton + "/shortlist?api_key=77a9113bda7ae5b92a6ef892135d4e04")
-        res.redirect('/candidates')
-        console.log(result)
-    }
-    catch(err) {
-        res.render("errors", {err})
-        console.log(err)
-    }
-})
 
 // Server Port Setup
 app.listen("3000", function () {
     console.log("Server started at port 3000.");
 });
+
+export default app
---
+++ index.test.js
@@ -0,0 +1,70 @@
+import request from "supertest";
+import app from './index.js';
+
+
+describe("POST /create-candidate", () => {
+    describe("given first name, last name and email", () => {
+        test("should respond with a 200 status code for success", async () => {
+            let x = Math.floor((Math.random() * 100_000_000) + 1)
+            const response = await request(app).post("/create-candidate").send(
+            {
+                "api_key": "77a9113bda7ae5b92a6ef892135d4e04",
+                "campaign_invitation": {
+                    "first_name": `first_name${x}`,
+                    "last_name": `last_name${x}`,
+                    "email": `email${x}@email.com`,
+                    "user_phone_number": "string",
+                    "source": "string", 
+                    "others": {}
+                }
+            })
+            expect(response.statusCode).toBe(200)
+        })
+    })
+})
+
+describe("GET /candidates", () => {
+    describe("retrieving the list of all candidates", () => {
+        test("should respond with a 200 status code", async () => {
+            const response = await request(app).get("/candidates")
+            expect(response.statusCode).toBe(200)
+        })
+    })
+})
+
+describe("PUT /shortlist", () => {
+    describe("using the put shortlist API without any specific candidate ID", () => {
+        test("should respond with a 404 status code", async () => {
+            const response = await request(app).get(`/shortlist`)
+            expect(response.statusCode).toBe(404)
+        })
+    })
+})
+
+describe("POST /campaign", () => {
+    describe("given basic campaign field requirements", () => {
+        test("should respond with a 200 status code for success", async () => {
+            const response = await request(app).post("/campaign").send(
+                {
+                    "api_key": "77a9113bda7ae5b92a6ef892135d4e04",
+                    "campaign": {
+                        "campaign_title": `Vince Ivan Minez campaign`,
+                        "job_title": "Junior Software Developer",
+                        "street": "Caballes Street",
+                        "city": "Tayug",
+                        "region": "Region 1",
+                        "postal_code": "2445",
+                        "country": "PH",
+                        "short_description": "string",
+                        "long_description": "string",
+                        "ats_external_id": "string",
+                        "job_type": "string",
+                        "salary": "string",
+                        "salary_type": "string",
+                        "salary_currency": "string"
+                    }
+                })
+            expect(response.statusCode).toBe(200)
+        })
+    })
+})
